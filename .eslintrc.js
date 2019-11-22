961ab1cd42fa090778b3a3fc7f6ef1e6dca04fad
ivandotv <390700+ivandotv@users.noreply.github.com>
2019-11-22 22:11:14 +0100

    add no-unused-vars ignore pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,5 +43,9 @@ module.exports = {
         },
       },
     ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
   },
 }

d7dd238a2d17086d6f1f6f30039c2886b3acbf32
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-01-18 00:02:50 +0100

    Add coverage treshold

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,13 @@ module.exports = {
     transform: {
       '^.+\\.tsx?$': 'ts-jest',
     },
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
   },
 }
